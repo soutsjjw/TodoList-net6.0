341251eacc23593acb7df9d11fce63b881dc995f
soutsjjw <souts.jjw@gmail.com>
2022-03-15 15:02:38 +0800

    使用.NET 6开发TodoList应用(12)——实现ActionFilter
    https://www.cnblogs.com/code4nothing/p/build-todolist-12.html

---
+++ src/TodoList.Api/Filters/LogFilterAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+
+namespace TodoList.Api.Filters;
+
+public class LogFilterAttribute : IActionFilter
+{
+    private readonly ILogger<LogFilterAttribute> _logger;
+
+    public LogFilterAttribute(ILogger<LogFilterAttribute> logger) => _logger = logger;
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        var action = context.RouteData.Values["action"];
+        var controller = context.RouteData.Values["controller"];
+        // 獲取名稱包含Command的參數值
+        var param = context.ActionArguments.SingleOrDefault(x => x.Value.ToString().Contains("Command")).Value;
+
+        _logger.LogInformation($"Controller:{controller}, action: {action}, Incoming request: {JsonSerializer.Serialize(param)}");
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        var action = context.RouteData.Values["action"];
+        var controller = context.RouteData.Values["controller"];
+        // 需要先將Result轉換為ObjectResult類型才能拿到Value值
+        var result = (ObjectResult)context.Result!;
+
+        _logger.LogInformation($"Controller:{controller}, action: {action}, Executing response: {JsonSerializer.Serialize(result.Value)}");
+    }
+}
+
--- src/TodoList.Api/Program.cs
+++ src/TodoList.Api/Program.cs
@@ -1,4 +1,5 @@
 using TodoList.Api.Extensions;
+using TodoList.Api.Filters;
 using TodoList.Application;
 using TodoList.Infrasturcture;
 using TodoList.Infrasturcture.Log;
@@ -14,6 +15,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<LogFilterAttribute>();
 
 // 添加應用層配置
 builder.Services.AddApplication();
